a2c16401dd288ccca7c5b6749d3f699c369f8342
Don Nguyen <ndon55555@gmail.com>
2021-04-05 23:14:50 -0400

    cleanup
    Co-authored-by: Cleverina Cong <cccongx@gmail.com>

--- browser/index.js
+++ browser/index.js
@@ -1,36 +1,25 @@
 function handleServerResponse() {
   if (http.readyState === XMLHttpRequest.DONE) {
-    // const p = document.getElementById("changeThis");
     const gameState = JSON.parse(http.responseText);
-    const grid = document.getElementById("gameGrid")
+    const grid = document.getElementById("gameGrid");
+    
     for(const child of grid.children) {
       const row = parseInt(child.getAttribute("row"));
       const col = parseInt(child.getAttribute("col"));
 
       child.innerText = gameState[row][col];
     }
-    //grid.innerHTML = ''; // Remove children
-
-    // console.log(resp)
-    // p.innerText = resp
   }
 }
 
 window.onload = function(){
     const rows = 10;
     const cols = 10;
-    const grid = document.getElementById("gameGrid")
+    const grid = document.getElementById("gameGrid");
 
     for(let i=0; i<rows; i++) {
       for(let j=0; j<cols; j++) {
-        let newDiv = document.createElement("div");
-        newDiv.setAttribute("class", "cell");
-        newDiv.setAttribute("row", i);
-        newDiv.setAttribute("col", j);
-        newDiv.onclick = function() {
-          openTile(i, j);
-        }
-        
+        const newDiv = createMinesweeperTile(i, j);
         console.log(newDiv);
         grid.appendChild(newDiv);
       }
@@ -47,4 +36,16 @@ function openTile(row, col) {
   http.open('POST', 'open', true);
   http.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   http.send("row="+row+"&col="+col);
+}
+
+function createMinesweeperTile(i, j) {
+  const newDiv = document.createElement("div");
+  newDiv.setAttribute("class", "cell");
+  newDiv.setAttribute("row", i);
+  newDiv.setAttribute("col", j);
+  newDiv.onclick = function() {
+    openTile(i, j);
+  }
+
+  return newDiv
 }
