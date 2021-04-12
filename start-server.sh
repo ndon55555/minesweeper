c135f0b43255a92c8e54bae40b3bd7f77f3bd5f8
Don Nguyen <ndon55555@gmail.com>
2021-04-12 03:16:25 -0400

    move server code into backend/ folder

--- start-server.sh
+++ start-server.sh
@@ -7,4 +7,7 @@ pushd "$(pwd)/frontend"
     npx webpack --env minesweeper_static_folder="$artifacts_dir"
 popd
 
+# Serve static files and start game server
+pushd "$(pwd)/backend"
 MINESWEEPER_STATIC_FOLDER="$artifacts_dir" gunicorn --worker-class=eventlet --workers=1 server:app
+popd
